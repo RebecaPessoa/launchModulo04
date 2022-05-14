3bd6045656d1fa708b666380b03c8880ddd7495c
RebecaPessoa <rebecahayanny17@gmail.com>
2022-05-14 01:10:58 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express') 
+const nunjucks = require('nunjucks') 
+const routes = require("./routes")
+
+const server = express() 
+
+server.use(express.static('public'))
+server.use(routes) //chamado middleware
+
+server.set("view engine", "njk") 
+
+nunjucks.configure("views", {
+    express:server,
+    autoescape: false, //evita que apareça injeção de html
+    noCache: true 
+})
+
+
+
+server.listen(5000, function(){ 
+    console.log("server is running")
+})
